c4541680b75f83a864a9d814b868f7a4f6051e1d
VanDuc <22130050@st.hcmuaf.edu.vn>
2026-05-30 22:30:49 +0700

    add get bank account api  for streamer setting

---
+++ src/main/java/org/example/donatebackend/dto/response/BankAccountResponse.java
@@ -0,0 +1,74 @@
+package org.example.donatebackend.dto.response;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import java.util.Date;
+
+public class BankAccountResponse {
+    private  Long id;
+    private Long streamerId;
+    private String providerType;
+    private String providerCode;
+    private String accountNo;
+    private String accountName;
+    private Date createdAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStreamerId() {
+        return streamerId;
+    }
+
+    public void setStreamerId(Long streamerId) {
+        this.streamerId = streamerId;
+    }
+
+    public String getProviderType() {
+        return providerType;
+    }
+
+    public void setProviderType(String providerType) {
+        this.providerType = providerType;
+    }
+
+    public String getProviderCode() {
+        return providerCode;
+    }
+
+    public void setProviderCode(String providerCode) {
+        this.providerCode = providerCode;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
